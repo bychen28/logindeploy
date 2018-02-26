9cc61cf960129d640dcbf51ffe01f009c3f85bc0
BryantChen <bychen28@gmail.com>
2018-02-26 10:16:25 -0800

    inital commit

---
+++ apps/djangologin/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views           # This line is new!
+urlpatterns = [
+	url(r'^$', views.index),     # This line has changed!
+	url(r'^/create$', views.create),
+	url(r'^/log$', views.login),
+	url(r'^/logg$', views.log),
+	url(r'^logout$',views.logout)
+]
